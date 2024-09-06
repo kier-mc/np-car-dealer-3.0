32b46d18cb528be612288cea67efd4d6544651a8
Kieran <kiermccarth+github@gmail.com>
2024-09-06 17:53:26 +0100

    FEAT: Add app entry point

---
+++ app/app.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { QUERY_PARAMS } from "~~/utils/api";
+
+useState("query-params", () => QUERY_PARAMS);
+</script>
+
+<template>
+  <NuxtLayout>
+    <slot />
+  </NuxtLayout>
+</template>
