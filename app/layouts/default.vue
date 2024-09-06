3dcabef9cad9aeb60a61506265976fb2c600f0a3
Kieran <kiermccarth+github@gmail.com>
2024-09-06 17:53:08 +0100

    FEAT: Add default layout

---
+++ app/layouts/default.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div id="top" class="layout">
+    <NuxtPage />
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use "styles:size";
+
+.layout {
+  display: grid;
+  max-width: size.breakpoint("desktop");
+  margin-inline: auto;
+}
+</style>
