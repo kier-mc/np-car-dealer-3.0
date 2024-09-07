3acd069b73d41f77a16b75931288f057bc603ae1
Kieran <kiermccarth+github@gmail.com>
2024-09-07 19:36:29 +0100

    FEAT: Add semantic label

--- app/components/feed/FeedSorting.vue
+++ app/components/feed/FeedSorting.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts">
 const { activeFilter, uiData } = useFeedSorting();
+const id = useId();
 </script>
 
 <template>
-  <UiSelect v-model:selected="activeFilter">
-    <option v-for="{ label, value } in uiData" :key="value" :value>
-      {{ label }}
-    </option>
-  </UiSelect>
+  <span>
+    <label class="label" :for="id">Sort results</label>
+    <UiSelect :id v-model:selected="activeFilter">
+      <option v-for="{ label, value } in uiData" :key="value" :value>
+        {{ label }}
+      </option>
+    </UiSelect>
+  </span>
 </template>
+
+<style scoped lang="scss">
+@use "mixins:ui";
+
+.label {
+  @include ui.sr-only();
+}
+</style>
