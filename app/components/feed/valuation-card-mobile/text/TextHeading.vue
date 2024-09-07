0835ba38fc0499d9ba3760b4d3835d48d19f18bd
Kieran <kiermccarth+github@gmail.com>
2024-09-07 14:10:33 +0100

    FEAT: Add mobile valuation card heading component

---
+++ app/components/feed/valuation-card-mobile/text/TextHeading.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h2 class="heading">
+    <slot />
+  </h2>
+</template>
+
+<style scoped lang="scss">
+@use "mixins:reset";
+
+.heading {
+  @include reset.heading();
+
+  line-height: 1em;
+  font-size: var(--sz-text-lg);
+  font-weight: 600;
+}
+</style>
