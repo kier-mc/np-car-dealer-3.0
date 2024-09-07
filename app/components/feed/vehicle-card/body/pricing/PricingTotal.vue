a823ceae4f3c8ec65768f704bdfc0b3249177944
Kieran <kiermccarth+github@gmail.com>
2024-09-07 13:50:06 +0100

    FEAT: Add total price component

---
+++ app/components/feed/vehicle-card/body/pricing/PricingTotal.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import type { Vehicle } from "~~/types/api";
+
+const data = inject("data") as Vehicle;
+
+const { price } = data;
+</script>
+
+<template>
+  <div class="total">
+    <span class="total__price">
+      {{ price }}
+    </span>
+    <UiLink class="total__calculate" variant="brand">
+      Calculate finance
+    </UiLink>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use "styles:size";
+
+.total {
+  line-height: 1em;
+
+  &__price {
+    font-variant-numeric: tabular-nums;
+    margin-inline-end: var(--sz-spacing-2xs);
+    font-size: var(--sz-text-xs);
+
+    &::before {
+      content: "£";
+    }
+  }
+
+  &__calculate {
+    color: var(--cl-brand-primary);
+    font-size: var(--sz-text-xs);
+    text-decoration: none;
+
+    @media (max-width: #{size.breakpoint("mobile")}) {
+      display: none;
+    }
+  }
+}
+</style>
