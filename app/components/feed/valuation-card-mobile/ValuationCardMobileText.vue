da3e3461d7172de04f678d6e20921f4550adf139
Kieran <kiermccarth+github@gmail.com>
2024-09-07 14:11:01 +0100

    FEAT: Add mobile valuation card text component

---
+++ app/components/feed/valuation-card-mobile/ValuationCardMobileText.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <FeedValuationCardMobileTextHeading>
+      <slot name="heading" />
+    </FeedValuationCardMobileTextHeading>
+    <FeedValuationCardMobileTextSubheading>
+      <slot name="subheading" />
+    </FeedValuationCardMobileTextSubheading>
+  </div>
+</template>
