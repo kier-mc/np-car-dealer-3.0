cce950a114d1d79234cea790ba460b5db6c47479
Kieran <kiermccarth+github@gmail.com>
2024-09-07 13:28:44 +0100

    FEAT: Add gallery image component

---
+++ app/components/feed/vehicle-card/header/gallery/GalleryImage.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+interface Props {
+  src?: string;
+};
+
+const props = withDefaults(defineProps<Props>(), {
+  src: "https://placehold.co/320x213?text=No+Image+Available",
+});
+const { src } = props;
+</script>
+
+<template>
+  <img class="image" :src alt="" loading="lazy" width="320" height="213">
+</template>
+
+<style scoped lang="scss">
+.image {
+  pointer-events: none;
+  inline-size: 100%;
+  block-size: 100%;
+  object-fit: cover;
+  object-position: top;
+}
+</style>
