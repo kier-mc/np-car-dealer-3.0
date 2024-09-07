8b6e08a28ab687d3b56ef2abdbeb230bca561db6
Kieran <kiermccarth+github@gmail.com>
2024-09-07 13:39:11 +0100

    FEAT: Add heading title component

---
+++ app/components/feed/vehicle-card/body/heading/HeadingTitle.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import type { Vehicle } from "~~/types/api";
+
+const data = inject("data") as Vehicle;
+
+const title = computed(() => {
+  const { make, model, plate } = data;
+  return `${plate} ${make} ${model}`;
+});
+</script>
+
+<template>
+  <h2 class="title">
+    {{ title }}
+  </h2>
+</template>
+
+<style scoped lang="scss">
+@use "mixins:reset";
+@use "styles:size";
+
+.title {
+  @include reset.heading();
+  line-height: calc(1em + 0.25em);
+
+  @media (max-width: #{size.breakpoint("mobile")}) {
+    line-height: calc(1em + 0.375em);
+    font-size: var(--sz-text-sm);
+  }
+}
+</style>
