b35d1db8f4d3cbbfeea1b477bb0d9b8a4ab89834
Kieran <kiermccarth+github@gmail.com>
2024-09-07 13:51:54 +0100

    FEAT: Add vehicle card component

---
+++ app/components/feed/FeedVehicleCard.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import type { Vehicle } from "~~/types/api";
+
+interface Props {
+  data: Vehicle;
+}
+
+const props = defineProps<Props>();
+const { data } = props;
+
+provide<Vehicle>("data", data);
+</script>
+
+<template>
+  <UiCard element="article" class="card">
+    <FeedVehicleCardHeader />
+    <FeedVehicleCardBody />
+  </UiCard>
+</template>
+
+<style scoped lang="scss">
+.card {
+  grid-row: span 3;
+  display: grid;
+  grid-template-rows: subgrid;
+  gap: 0rem;
+}
+</style>
