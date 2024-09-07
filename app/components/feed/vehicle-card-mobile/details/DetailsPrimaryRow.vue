f75d30645737ea97313a2cc7f56c94f7f9a8f8bf
Kieran <kiermccarth+github@gmail.com>
2024-09-07 13:57:43 +0100

    FEAT: Add mobile primary info row component

---
+++ app/components/feed/vehicle-card-mobile/details/DetailsPrimaryRow.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="primary">
+    <FeedVehicleCardBodyHeadingTitle class="primary__title" />
+    <FeedVehicleCardBodyHeadingSpec class="primary__spec" />
+    <FeedVehicleCardHeaderCondition class="primary__condition" />
+    <FeedVehicleCardBodyFavouriteButton class="primary__favourite" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.primary {
+  display: grid;
+  grid-template-columns: 1fr auto auto;
+  grid-template-rows: repeat(2, auto);
+  column-gap: var(--sz-spacing-xs);
+
+  &__title {
+    align-self: center;
+    grid-column: 1;
+    grid-row: 1;
+  }
+
+  &__spec {
+    grid-column: 1;
+    grid-row: 2;
+  }
+
+  &__condition {
+    align-self: center;
+    grid-column: 2;
+    grid-row: 1;
+  }
+
+  &__favourite {
+    grid-column: 3;
+    grid-row: 1;
+  }
+}
+</style>
