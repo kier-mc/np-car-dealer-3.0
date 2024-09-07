35021a6b619668d009b25cc4ccba84cd794f8b0b
Kieran <kiermccarth+github@gmail.com>
2024-09-07 13:28:02 +0100

    FEAT: Add vehicle condition component

---
+++ app/components/feed/vehicle-card/header/HeaderCondition.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import type { Vehicle } from "~~/types/api";
+
+const data = inject("data") as Vehicle;
+const { advert_classification } = data;
+</script>
+
+<template>
+  <UiChip class="condition">
+    <span class="condition__label">{{ advert_classification }}</span>
+  </UiChip>
+</template>
+
+<style scoped lang="scss">
+@use "styles:size";
+
+.condition {
+  align-self: start;
+  padding-inline: var(--sz-spacing-xs);
+  padding-block: var(--sz-spacing-3xs);
+  border-radius: var(--sz-radius-xs);
+
+  @media (max-width: #{size.breakpoint("mobile")}) {
+    padding-block: 0rem;
+  }
+
+  &__label {
+    color: var(--cl-text-light);
+    font-weight: 500;
+    text-transform: lowercase;
+
+    &::first-letter {
+      text-transform: uppercase;
+    }
+
+    @media (max-width: #{size.breakpoint("mobile")}) {
+      font-size: var(--sz-text-xs);
+      font-weight: initial;
+    }
+  }
+}
+</style>
