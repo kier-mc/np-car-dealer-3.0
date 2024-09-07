e9afe5eb21424d9089b38c38bf5b454fe96e1157
Kieran <kiermccarth+github@gmail.com>
2024-09-07 13:30:56 +0100

    FEAT: Add gallery navigation component

---
+++ app/components/feed/vehicle-card/header/gallery/GalleryNavigation.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="navigation">
+    <FeedVehicleCardHeaderGalleryNavigationCount />
+    <FeedVehicleCardHeaderGalleryNavigationControls />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.navigation {
+  grid-column: 1;
+  grid-row: 1;
+  justify-self: end;
+  align-self: start;
+  display: flex;
+  align-items: center;
+  gap: var(--sz-spacing-xs);
+  margin: var(--sz-spacing-xs);
+}
+</style>
