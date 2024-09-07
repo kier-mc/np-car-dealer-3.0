1e344abc6aa601d71a5dac24e1c08d9849d04f2d
Kieran <kiermccarth+github@gmail.com>
2024-09-07 12:17:26 +0100

    FEAT: Add feed sorting component

---
+++ app/components/feed/FeedSorting.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const { activeFilter, uiData } = useFeedSorting();
+</script>
+
+<template>
+  <UiSelect v-model:selected="activeFilter">
+    <option v-for="{ label, value } in uiData" :key="value" :value>
+      {{ label }}
+    </option>
+  </UiSelect>
+</template>
