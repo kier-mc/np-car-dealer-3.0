4b614d174815af07a5ddba4a81e252add68cb6ea
Kieran <kiermccarth+github@gmail.com>
2024-09-07 14:01:43 +0100

    FEAT: Add mobile vehicle card component

---
+++ app/components/feed/FeedVehicleCardMobile.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import type { Vehicle } from "~~/types/api";
+
+interface Props {
+  data: Vehicle;
+}
+
+const props = defineProps<Props>();
+const { data } = props;
+
+provide<Vehicle>("data", data);
+</script>
+
+<template>
+  <UiCard element="article" :shadow="false" class="card">
+    <FeedVehicleCardMobileGallery />
+    <FeedVehicleCardMobileDetails />
+  </UiCard>
+</template>
+
+<style scoped lang="scss">
+.card {
+  display: grid;
+  gap: var(--sz-spacing-lg);
+}
+</style>
