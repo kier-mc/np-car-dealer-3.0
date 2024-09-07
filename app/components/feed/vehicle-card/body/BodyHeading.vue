04fb03bacded7bd1019935107fe9b73555629934
Kieran <kiermccarth+github@gmail.com>
2024-09-07 13:40:01 +0100

    FEAT: Add heading component

---
+++ app/components/feed/vehicle-card/body/BodyHeading.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="heading">
+    <FeedVehicleCardBodyHeadingTitle />
+    <FeedVehicleCardBodyHeadingSpec />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.heading {
+  display: grid;
+  grid-template-rows: repeat(2, auto);
+}
+</style>
