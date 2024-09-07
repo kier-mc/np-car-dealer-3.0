a51bb97ed4a46ce3107e4d71ae78de58a07193ea
Kieran <kiermccarth+github@gmail.com>
2024-09-07 12:20:59 +0100

    FEAT: Add result count component

---
+++ app/components/feed/header/HeaderCount.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import type { Metadata } from "~~/types/api";
+
+const meta = useState<Metadata>("api-meta");
+
+const vehicleCount = computed(() => {
+  const { total } = meta.value;
+  return total > 1 ? `${total} cars` : `${total} car`;
+});
+</script>
+
+<template>
+  <span class="count">
+    {{ vehicleCount }}
+  </span>
+</template>
+
+<style scoped lang="scss">
+@use "styles:size";
+
+.count {
+  font-variant-numeric: tabular-nums;
+  font-weight: 600;
+
+  &::before {
+    content: "Showing ";
+  }
+
+  @media (max-width: #{size.breakpoint("desktop")}) {
+    &::before {
+      content: "";
+    }
+  }
+}
+</style>
