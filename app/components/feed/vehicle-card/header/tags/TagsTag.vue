0f555fc5a048a64c6dfebe797df5e52971f1f19c
Kieran <kiermccarth+github@gmail.com>
2024-09-07 13:35:54 +0100

    FEAT: Add tag component

---
+++ app/components/feed/vehicle-card/header/tags/TagsTag.vue
@@ -0,0 +1,27 @@
+<template>
+  <UiChip class="tag">
+    <span class="tag__label">
+      <slot />
+    </span>
+  </UiChip>
+</template>
+
+<style scoped lang="scss">
+.tag {
+  align-self: end;
+  padding-inline: var(--sz-spacing-xs);
+  padding-block: var(--sz-spacing-2xs);
+  border-radius: var(--sz-radius-xs);
+
+  &__label {
+    line-height: 1em;
+    color: var(--cl-text-light);
+    font-size: var(--sz-text-xs);
+    text-transform: lowercase;
+
+    &::first-letter {
+      text-transform: uppercase;
+    }
+  }
+}
+</style>
