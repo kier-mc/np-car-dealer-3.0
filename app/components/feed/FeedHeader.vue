da4a64ad815dd014d44f24327673f2361f3c046c
Kieran <kiermccarth+github@gmail.com>
2024-09-07 13:15:37 +0100

    FEAT: Add feed header component

---
+++ app/components/feed/FeedHeader.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="header">
+    <FeedHeaderCount class="header__count" />
+    <FeedHeaderFilters class="header__filters" />
+    <FeedSorting class="header__sorting" />
+  </header>
+</template>
+
+<style scoped lang="scss">
+@use "styles:size";
+
+.header {
+  display: grid;
+  grid-template-columns: auto 1fr auto;
+  align-items: center;
+  gap: var(--sz-spacing-sm);
+
+  &__count {
+    margin-inline-end: var(--sz-spacing-lg);
+
+    @media (max-width: #{size.breakpoint("tablet")}) {
+      margin-inline-end: 0rem;
+    }
+  }
+}
+</style>
