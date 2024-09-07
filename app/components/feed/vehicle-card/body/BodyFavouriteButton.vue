a670eb63f66061a57f018a58d9975ffa15c72bfc
Kieran <kiermccarth+github@gmail.com>
2024-09-07 13:41:24 +0100

    FEAT: Add favourite button component

---
+++ app/components/feed/vehicle-card/body/BodyFavouriteButton.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+const isFavourite = ref<boolean>(false);
+
+function toggleFavourite() {
+  isFavourite.value = !isFavourite.value;
+};
+</script>
+
+<template>
+  <button class="button" type="button" @click="toggleFavourite()">
+    <Transition mode="out-in">
+      <UiIcon v-if="isFavourite" colour="var(--cl-brand-primary)" icon="star-20-solid" />
+      <UiIcon v-else icon="star" />
+    </Transition>
+  </button>
+</template>
+
+<style scoped lang="scss">
+@use "mixins:reset";
+@use "mixins:ui";
+
+.button {
+  @include reset.button();
+  display: grid;
+  position: relative;
+
+  &::before {
+    @include ui.button-focus();
+
+    border-radius: 50%;
+    margin: (calc(var(--sz-border-xl) * -1));
+  }
+
+  &:focus-visible::before {
+    opacity: 1;
+  }
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: opacity var(--tm-150) var(--ef-out-quart);
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
