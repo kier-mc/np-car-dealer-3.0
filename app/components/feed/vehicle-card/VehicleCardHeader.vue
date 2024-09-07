4e9634a9fefc6595b5fdd84c181c6697c30b06bf
Kieran <kiermccarth+github@gmail.com>
2024-09-07 13:36:45 +0100

    FEAT: Add card header component

---
+++ app/components/feed/vehicle-card/VehicleCardHeader.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="header">
+    <FeedVehicleCardHeaderCondition class="header__condition" />
+    <FeedVehicleCardHeaderGallery class="header__gallery" />
+    <FeedVehicleCardHeaderTags class="header__tags" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.header {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: auto 1fr auto;
+
+  &::before {
+    grid-column: 1;
+    grid-row: 1;
+    content: "";
+    border-radius: var(--sz-radius-md) var(--sz-radius-md) 0 0;
+    background-image: linear-gradient(180deg, var(--cl-fw-card-fade), hsl(0, 0%, 100%, 0));
+  }
+
+  &__condition {
+    grid-column: 1;
+    grid-row: 1;
+    z-index: 10;
+    margin: var(--sz-spacing-xs);
+  }
+
+  &__gallery {
+    grid-column: 1;
+    grid-row: 1 / -1;
+  }
+
+  &__tags {
+    grid-column: 1;
+    grid-row: 3;
+    z-index: 10;
+    margin: var(--sz-spacing-xs);
+  }
+}
+</style>
