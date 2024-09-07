20e64dc9b535e5ab507c6afcb5e7c925df595a6d
Kieran <kiermccarth+github@gmail.com>
2024-09-07 13:17:42 +0100

    FEAT: Add mobile result count component

---
+++ app/components/feed/header-mobile/HeaderMobileCount.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type { Metadata } from "~~/types/api";
+
+const meta = useState<Metadata>("api-meta");
+
+const vehicleCount = computed(() => {
+  const { per_page, total } = meta.value;
+  const count = per_page >= total ? total : per_page;
+  return total > 1 ? `Showing ${count} of ${total} cars` : `Showing ${count} of ${total} car`;
+});
+</script>
+
+<template>
+  <span class="count">
+    {{ vehicleCount }}
+  </span>
+</template>
+
+<style scoped lang="scss">
+.count {
+  font-variant-numeric: tabular-nums;
+  font-size: var(--sz-text-sm);
+}
+</style>
