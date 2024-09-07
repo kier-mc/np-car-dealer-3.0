f5616c6c43e53ea57b419a13b87da9a20b65e817
Kieran <kiermccarth+github@gmail.com>
2024-09-07 18:45:23 +0100

    FEAT: Add mobile listing feed component

---
+++ app/components/ListingFeedMobile.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const { results } = useFeedSorting();
+</script>
+
+<template>
+  <div class="feed">
+    <template v-for="(vehicle, index) in results" :key="vehicle._key">
+      <template v-if="index === 4">
+        <FeedValuationCardMobile />
+        <FeedVehicleCardMobile :data="vehicle" />
+      </template>
+      <FeedVehicleCardMobile v-else :data="vehicle" />
+    </template>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use "styles:size";
+
+.feed {
+  display: grid;
+  grid-template-columns: repeat(1, auto);
+  grid-template-rows: auto auto auto auto 1fr 1fr;
+  gap: var(--sz-spacing-xl);
+  margin-block-end: var(--sz-spacing-2xl);
+}
+</style>
