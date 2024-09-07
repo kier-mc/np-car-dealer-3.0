4504d4519200e43903969bdbd9501ad179bce0f3
Kieran <kiermccarth+github@gmail.com>
2024-09-07 14:08:23 +0100

    FEAT: Add valuation card button component

---
+++ app/components/feed/valuation-card/ValuationCardButton.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+  <UiButton class="button" variant="brand">
+    <slot />
+  </UiButton>
+</template>
+
+<style scoped lang="scss">
+@use "mixins:ui";
+
+.button {
+  --border-radius: var(--sz-radius-md);
+
+  max-width: 100%;
+  padding-inline: var(--sz-spacing-sm);
+  padding-block: var(--sz-spacing-sm);
+  border-radius: var(--border-radius);
+
+  &::before {
+    @include ui.button-focus($parent-border-radius: var(--border-radius));
+  }
+}
+</style>
