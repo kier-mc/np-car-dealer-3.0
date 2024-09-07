ce125ece4f639c33e8e4e0a41dfe4f83d71d0559
Kieran <kiermccarth+github@gmail.com>
2024-09-07 13:25:20 +0100

    FEAT: Add card component

---
+++ layers/ui/components/layout/Card.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+interface Props {
+  element?: string;
+  shadow?: boolean;
+};
+
+const props = withDefaults(defineProps<Props>(), {
+  element: "div",
+  shadow: true,
+});
+const { element } = props;
+
+const getCardClass = computed(() => {
+  const { shadow } = props;
+  return shadow ? "card card--shadow" : "card";
+});
+</script>
+
+<template>
+  <component :is="element" :class="getCardClass">
+    <slot />
+  </component>
+</template>
+
+<style scoped lang="scss">
+.card {
+  border-radius: var(--sz-radius-md);
+
+  &--shadow {
+    box-shadow: 0px 8px 24px 0px var(--cl-fw-shadow);
+  }
+}
+</style>
