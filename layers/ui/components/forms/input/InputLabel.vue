e7aa91f98f2fc1c7db25860bd9bb0000e4a0f858
Kieran <kiermccarth+github@gmail.com>
2024-09-07 14:03:11 +0100

    FEAT: Add input label component

---
+++ layers/ui/components/forms/input/InputLabel.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+interface Props {
+  required?: boolean;
+};
+
+const props = withDefaults(defineProps<Props>(), {
+  required: false,
+});
+
+const getLabelClass = computed(() => {
+  const { required } = props;
+  return required ? "label label--required" : "label";
+});
+</script>
+
+<template>
+  <label :class="getLabelClass">
+    <slot />
+  </label>
+</template>
+
+<style scoped lang="scss">
+.label {
+  pointer-events: none;
+  user-select: none;
+  display: flex;
+  width: max-content;
+  height: max-content;
+  padding-inline: var(--sz-spacing-xs);
+  border-radius: var(--sz-radius-sm);
+  margin-inline-start: var(--sz-spacing-xs);
+  margin-block-start: calc(var(--sz-spacing-sm) * -1);
+  background-color: var(--cl-fw-light);
+  font-size: var(--sz-text-xs);
+
+  &--required::after {
+    content: "*";
+    margin-inline-start: var(--sz-spacing-2xs);
+    color: var(--cl-danger-primary);
+  }
+}
+</style>
