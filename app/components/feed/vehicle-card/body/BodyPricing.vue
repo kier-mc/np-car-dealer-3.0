ebbe0cddad59e6cb5456d99ab29384de8c13e340
Kieran <kiermccarth+github@gmail.com>
2024-09-07 13:50:38 +0100

    FEAT: Add pricing component

---
+++ app/components/feed/vehicle-card/body/BodyPricing.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="pricing">
+    <FeedVehicleCardBodyPricingFinance class="pricing__finance" />
+    <FeedVehicleCardBodyPricingTotal class="pricing__total" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.pricing {
+  display: grid;
+
+  &__finance {
+    grid-column: 1;
+    grid-row: 1;
+  }
+
+  &__total {
+    grid-column: 1;
+    grid-row: 2;
+  }
+}
+</style>
