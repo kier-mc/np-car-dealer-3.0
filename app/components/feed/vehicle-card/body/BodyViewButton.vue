a79225712725ea6087ae18a6bd6cfe6f2e928ed6
Kieran <kiermccarth+github@gmail.com>
2024-09-07 13:51:01 +0100

    FEAT: Add view button component

---
+++ app/components/feed/vehicle-card/body/BodyViewButton.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts"></script>
+
+<template>
+  <UiButton class="button" variant="brand">
+    View
+  </UiButton>
+</template>
+
+<style scoped lang="scss">
+@use "mixins:ui";
+
+.button {
+  --border-radius: var(--sz-radius-md);
+
+  padding-block: var(--sz-spacing-xs);
+  border-radius: var(--border-radius);
+
+  &::before {
+    @include ui.button-focus($parent-border-radius: var(--border-radius));
+  }
+}
+</style>
