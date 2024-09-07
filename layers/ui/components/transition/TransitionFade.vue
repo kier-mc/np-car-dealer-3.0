cdc3c47150fe83ace589bf02efa60f0be88eeaf3
Kieran <kiermccarth+github@gmail.com>
2024-09-07 18:40:18 +0100

    FEAT: Add transition fade component

---
+++ layers/ui/components/transition/TransitionFade.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <Transition>
+    <slot />
+  </Transition>
+</template>
+
+<style scoped lang="scss">
+.v-enter-active,
+.v-leave-active {
+  transition: opacity var(--tm-150) var(--ef-out-quart);
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
