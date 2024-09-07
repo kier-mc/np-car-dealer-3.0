911a12ce360a4b1e0ef0d3384ed42a09ff715727
Kieran <kiermccarth+github@gmail.com>
2024-09-07 14:00:04 +0100

    FEAT: Add mobile details component

---
+++ app/components/feed/vehicle-card-mobile/VehicleCardMobileDetails.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="details">
+    <FeedVehicleCardMobileDetailsPrimaryRow />
+    <FeedVehicleCardMobileDetailsSecondaryRow />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.details {
+  display: grid;
+  gap: var(--sz-spacing-sm);
+  padding-inline: var(--sz-spacing-sm);
+}
+</style>
