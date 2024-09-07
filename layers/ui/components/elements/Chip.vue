f4973d870b8fc27fe3e02451e15ee0f6b0f5f814
Kieran <kiermccarth+github@gmail.com>
2024-09-07 13:25:29 +0100

    FEAT: Add chip component

---
+++ layers/ui/components/elements/Chip.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <span class="chip">
+    <slot />
+  </span>
+</template>
+
+<style scoped lang="scss">
+.chip {
+  display: inline-flex;
+  max-width: max-content;
+  border: var(--sz-border-px) solid var(--cl-fw-border-alpha);
+  background-color: var(--cl-fw-dark);
+}
+</style>
