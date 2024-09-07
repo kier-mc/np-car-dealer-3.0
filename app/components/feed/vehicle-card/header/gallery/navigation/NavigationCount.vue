b0611345c18c089e3f8d058c198f2a5fce4eba8e
Kieran <kiermccarth+github@gmail.com>
2024-09-07 13:29:32 +0100

    FEAT: Add gallery navigation count component

---
+++ app/components/feed/vehicle-card/header/gallery/navigation/NavigationCount.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { Gallery } from "../../HeaderGallery.vue";
+
+const { current, total } = inject("gallery") as Gallery;
+</script>
+
+<template>
+  <span class="count">
+    {{ current + 1 }} of {{ total }}
+  </span>
+</template>
+
+<style scoped lang="scss">
+.count {
+  color: var(--cl-text-light);
+  font-size: var(--sz-text-xs);
+  font-variant-numeric: tabular-nums;
+}
+</style>
