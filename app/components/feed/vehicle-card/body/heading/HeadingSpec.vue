70403b9e368f3676f9c8ee344a1c78c51f36bf31
Kieran <kiermccarth+github@gmail.com>
2024-09-07 13:39:36 +0100

    FEAT: Add heading specifications component

---
+++ app/components/feed/vehicle-card/body/heading/HeadingSpec.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import type { Vehicle } from "~~/types/api";
+
+const data = inject("data") as Vehicle;
+
+const spec = computed(() => {
+  const { derivative } = data;
+  return derivative;
+});
+</script>
+
+<template>
+  <span class="spec">
+    {{ spec }}
+  </span>
+</template>
+
+<style scoped lang="scss">
+@use "styles:size";
+
+.spec {
+  line-height: 1.5em;
+  color: var(--cl-text-mid);
+  font-size: var(--sz-text-xs);
+
+  @media (max-width: #{size.breakpoint("mobile")}) {
+    line-height: 1em;
+  }
+}
+</style>
