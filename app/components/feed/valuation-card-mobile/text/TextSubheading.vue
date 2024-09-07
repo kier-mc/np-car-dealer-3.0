63d277df08eeeacfb005e7e9d7d818dcf2f8893e
Kieran <kiermccarth+github@gmail.com>
2024-09-07 14:10:40 +0100

    FEAT: Add mobile valuation card subheading component

---
+++ app/components/feed/valuation-card-mobile/text/TextSubheading.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <span class="subheading">
+    <slot />
+  </span>
+</template>
+
+<style scoped lang="scss">
+.subheading {
+  line-height: 1em;
+  font-size: var(--sz-text-xs);
+}
+</style>
