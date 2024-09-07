55b36de4665012c492c6d82ae86bd2bef76b7854
Kieran <kiermccarth+github@gmail.com>
2024-09-07 13:56:54 +0100

    FEAT: Add mobile gallery component

---
+++ app/components/feed/vehicle-card-mobile/VehicleCardMobileGallery.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import type { Vehicle } from "~~/types/api";
+
+const data = inject("data") as Vehicle;
+const { media_urls } = data;
+
+const images = computed(() => {
+  return media_urls.slice(0, -5);
+});
+</script>
+
+<template>
+  <div class="gallery">
+    <FeedVehicleCardHeaderGalleryImage v-for="{ thumb } in images" :key="thumb" class="gallery__image" :src="thumb" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.gallery {
+  overflow-x: scroll;
+  display: flex;
+  gap: var(--sz-spacing-2xs);
+  padding-inline: var(--sz-spacing-sm);
+
+  &__image {
+    flex: 0 0 auto;
+    max-inline-size: 32%;
+    border-radius: var(--sz-radius-md);
+  }
+}
+</style>
