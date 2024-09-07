123af78cf92ba658b5c77ec7800c131cfee43770
Kieran <kiermccarth+github@gmail.com>
2024-09-07 13:10:22 +0100

    FEAT: Add header filters component

---
+++ app/components/feed/header/HeaderFilters.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const { setAdvertClassification, matchesAdvertClassification, uiData } = useQueryParams();
+
+function setVariant(classification: typeof uiData[number]) {
+  return matchesAdvertClassification(classification) ? "brand" : "outline";
+}
+</script>
+
+<template>
+  <div class="filters">
+    <UiButton v-for="filter in uiData" :key="filter" :variant="setVariant(filter)" @click="setAdvertClassification(filter)">
+      {{ filter }}
+    </UiButton>
+    <UiButton is-disabled>
+      Offers
+    </UiButton>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use "mixins:reset";
+@use "styles:size";
+
+.filters {
+  display: grid;
+  grid-auto-flow: column;
+  gap: var(--sz-spacing-2xs);
+  max-width: max-content;
+}
+</style>
