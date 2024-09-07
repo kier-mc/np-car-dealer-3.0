844810e3123d595bc20cfd041f3be3cd159118c2
Kieran <kiermccarth+github@gmail.com>
2024-09-07 14:08:05 +0100

    FEAT: Add valuation card header component

---
+++ app/components/feed/valuation-card/ValuationCardHeader.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="header">
+    <h2 class="header__title">
+      Value your car
+    </h2>
+    <span>Find out the value of your car in just a few minutes.</span>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use "mixins:reset";
+
+.header {
+  display: grid;
+  row-gap: var(--sz-spacing-sm);
+  text-align: center;
+
+  &__title {
+    @include reset.heading();
+
+    line-height: 1em;
+    font-size: var(--sz-text-xl);
+    font-weight: 500;
+  }
+}
+</style>
