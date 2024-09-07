6c806660c3838f0f382ba7433511e1565997152a
Kieran <kiermccarth+github@gmail.com>
2024-09-07 13:19:04 +0100

    FEAT: Add mobile header filters component

---
+++ app/components/feed/header-mobile/HeaderMobileFilters.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const { setAdvertClassification, matchesAdvertClassification, uiData } = useQueryParams();
+</script>
+
+<template>
+  <div class="filters">
+    <FeedHeaderMobileFiltersButton v-for="filter in uiData" :key="filter" :is-active="matchesAdvertClassification(filter)" @click="setAdvertClassification(filter)">
+      {{ filter }}
+    </FeedHeaderMobileFiltersButton>
+    <FeedHeaderMobileFiltersButton disabled>
+      Offers
+    </FeedHeaderMobileFiltersButton>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.filters {
+  display: grid;
+  grid-auto-flow: column;
+  grid-template-columns: repeat(4, 1fr);
+}
+</style>
